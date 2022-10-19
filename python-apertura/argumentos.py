de1c8a0b346ee993d81ac721075e84d9d050b15c
DavidSv00 <111893490+DavidSv00@users.noreply.github.com>
2022-10-18 23:24:42 -0500

    Argumentos python

---
+++ python-apertura/argumentos.py
@@ -0,0 +1,11 @@
+#Bibliotecas
+import argparse
+
+# Parser
+parser = argparse.ArgumentParser()
+parser.add_argument("img_src", help="Imagen a buscar en la BD de caras")
+parser.add_argument("db_path", help="Ruta de la base de datos de caras")
+args = parser.parse_args()
+
+ruta = args.img_src + "" + args.db_path
+print("Las rutas recibidas son:" + ruta)
